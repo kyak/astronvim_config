ebe77569e64049fc73fbd401b05249174a49c43e
kyak <699631+kyak@users.noreply.github.com>
2024-05-07 17:55:26 +0300

    Russian langmap

--- lua/polish.lua
+++ lua/polish.lua
@@ -1,18 +1,14 @@
-if true then return end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- This will run last in the setup process and is a good place to configure
 -- things like custom filetypes. This just pure lua so anything that doesn't
 -- fit in the normal config locations above can go here
 
--- Set up custom filetypes
-vim.filetype.add {
-  extension = {
-    foo = "fooscript",
-  },
-  filename = {
-    ["Foofile"] = "fooscript",
-  },
-  pattern = {
-    ["~/%.config/foo/.*"] = "fooscript",
-  },
-}
+vim.cmd([[
+    set langmap=ёйцукенгшщзхъфывапролджэячсмитьбюЁЙЦУКЕНГШЩЗХЪФЫВАПРОЛДЖЭЯЧСМИТЬБЮ;`qwertyuiop[]asdfghjkl\\;'zxcvbnm\\,.~QWERTYUIOP{}ASDFGHJKL:\\"ZXCVBNM<>
+    nmap Ж :
+    nmap Н Y
+    nmap з p
+    nmap ф a
+    nmap щ o
+    nmap г u
+    nmap З P
+]])
